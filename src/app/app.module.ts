74560818062e35971598f902c880229f4ef9f219
sergey <sergeykrohin86@gmail.com>
2019-06-03 16:39:58 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-//import { HttpModule } from '@angular/http';
-//import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DashboardModule } from './modules/dashboard/dashboard.module';
 import { CoreModule } from './modules/core/core.module';
@@ -10,8 +8,6 @@ import { CoreModule } from './modules/core/core.module';
     AppComponent
   ],
   imports: [
-    /*HttpModule,*/
-	/*HttpClientModule,*/
 	DashboardModule,
 	CoreModule
   ],
